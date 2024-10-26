8ae6c7f683167a3bdea4cfdede96eede59a13bf9
Jan Rafanan <yanidruffy@gmail.com>
2024-10-26 19:54:13 +0000

    feat: set up allauth

--- urban_odyssey/settings.py
+++ urban_odyssey/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,9 @@ SECRET_KEY = 'django-insecure-fre)sls2k#)zxup26qhvagcbo0-xa-_w#(x6(g(p*5o+g1bbk_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-yanidruffy-urbanodyssey-fsbz4jb5ydb.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-yanidruffy-urbanodyssey-fsbz4jb5ydb.ws.codeinstitute-ide.net',]
 
+CSRF_TRUSTED_ORIGINS = ['https://8000-yanidruffy-urbanodyssey-fsbz4jb5ydb.ws.codeinstitute-ide.net',]
 
 # Application definition
 
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +72,22 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+SITE_ID = 1
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'urban_odyssey.wsgi.application'
 
