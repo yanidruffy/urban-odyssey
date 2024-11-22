7a96d71868c32dabc243d7449f1402433d196d2d
Jan Rafanan <yanidruffy@gmail.com>
2024-11-22 14:40:45 +0000

    feat: add navigation links to use correct URLS for categories, 'All Products' and home

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='category_product_list'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,11 +3,13 @@ from .models import Category, Product
 
 # Create your views here.
 
-def product_list(request):
+def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
     context = {
         'category': category,
         'categories': categories,
