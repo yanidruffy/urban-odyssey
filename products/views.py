e074d7780bb03a4ce0ad8bc3604bfe009a3e204a
Jan Rafanan <yanidruffy@gmail.com>
2024-11-22 12:33:42 +0000

    feat: add product_detail page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 # Create your views here.
@@ -14,4 +14,13 @@ def product_list(request):
         'products': products,
     }
 
-    return render(request, 'products/list.html', context)
+    return render(request, 'products/product_list.html', context)
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
