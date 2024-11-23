5df3410a9dde2bda6a42d61c187c0a14d319ef1f
Jan Rafanan <yanidruffy@gmail.com>
2024-11-23 10:35:56 +0000

    feat: add bag urls and template

--- urban_odyssey/urls.py
+++ urban_odyssey/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('bag/', include('bag.urls', namespace='bag')),
     path('products/', include('products.urls', namespace='products')),
     path('', include('home.urls', namespace='home')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
