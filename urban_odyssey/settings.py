05a91329bbe2d67a72dea83f7a489863b7d344bd
Jan Rafanan <yanidruffy@gmail.com>
2024-11-24 08:16:37 +0000

    feat: add bag contexts and delivery y logic

--- urban_odyssey/settings.py
+++ urban_odyssey/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -153,3 +154,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STANDARD_DELIVERY_FEE = 30
+FREE_DELIVERY_THRESHOLD = 100
