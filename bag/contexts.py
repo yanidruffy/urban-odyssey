8955c3bb3f9646badb04a8ed830953267a1709f9
Jan Rafanan <yanidruffy@gmail.com>
2024-11-27 08:16:29 +0000

    feat: add product details, totals and quantity boxes to bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,20 +4,25 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 
 def bag_contents(request):
+    bag = request.session.get('bag', {})
+    print("Session bag:", bag)
+
     bag_items = []
     total = 0
     product_count = 0
-    bag = request.session.get('bag', {})
 
-    for item_id, item_data in bag.items():
-        product = get_object_or_404(Product, id=item_id)
-        total += item_data * product.price
-        product_count += item_data
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': item_data,
-            'product': product,
-        })
+    if not bag:
+        print("Bag is empty.")
+    else:
+        for item_id, item_data in bag.items():
+            product = get_object_or_404(Product, id=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
 
     if total == 0:
         delivery = 0
