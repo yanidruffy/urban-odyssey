5a47e3e1bbaced670fcde07348e9da760bdb5e04
yanidruffy <111918613+yanidruffy@users.noreply.github.com>
2024-12-07 14:20:38 +0100

    fix: correct DEBUG setting based on DEVELOPMENT environment variable

--- urban_odyssey/settings.py
+++ urban_odyssey/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = os.environ.get('DEVELOPMENT', 'False') == 'True'
 
 ALLOWED_HOSTS = [
 	'8000-yanidruffy-urbanodyssey-fsbz4jb5ydb.ws.codeinstitute-ide.net',
