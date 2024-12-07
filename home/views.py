fe3d33a8846dc44217e588d12ba4787d57ce3c6d
yanidruffy <111918613+yanidruffy@users.noreply.github.com>
2024-12-08 00:06:02 +0100

    feat: add top rated products to home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from about.models import About
 from faq.models import Faq
+from products.models import Product
 
 # Create your views here.
 
@@ -8,9 +9,11 @@ def index(request):
 	""" A view to return the index page """
 	about = About.objects.first()
 	faqs = Faq.objects.filter(published=True).order_by('-created')[:3]
+	top_products = Product.objects.filter(available=True, rating__isnull=False).order_by('-rating')[:3]
 
 	context = {
 		'about': about,
 		'faqs': faqs,
+		'top_products': top_products,
 	}
 	return render(request, 'home/index.html', context)
