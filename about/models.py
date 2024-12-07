9718339ff8a265cc670bf511164acaed2590e984
yanidruffy <111918613+yanidruffy@users.noreply.github.com>
2024-12-07 23:23:13 +0100

    feat: add company statement and mission to html files

--- about/models.py
+++ about/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class About(models.Model):
 	title=models.CharField(max_length=100)
-	mission=models.TextField(default="Our mission is to empower digital nomads and adventurers with stylish, durable, and minimalist products.")
 	history=models.TextField(default="Founded with the goal of redefining urban mobility, we partner with leading brands to deliver innovative solutions.")
+	mission=models.TextField(default="Our mission is to empower digital nomads and adventurers with stylish, durable, and minimalist products.")
 	vision=models.TextField(default="To become the go-to platform for digital nomads seeking premium everyday carry solutions.")
+	company_statement = models.TextField(default="Empowering nomads and redefining urban mobility.")
 	updated_at=models.DateTimeField(auto_now=True)
 
 	class Meta:
--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from about.models import About
 from faq.models import Faq
 
 # Create your views here.
 
 def index(request):
+	""" A view to return the index page """
+	about = About.objects.first()
 	faqs = Faq.objects.filter(published=True).order_by('-created')[:3]
-	return render(request, 'home/index.html', {'faqs': faqs})
+
+	context = {
+		'about': about,
+		'faqs': faqs,
+	}
+	return render(request, 'home/index.html', context)
