cb623045e7eb0d4fe4285c7c21ceedf279af80bf
yanidruffy <111918613+yanidruffy@users.noreply.github.com>
2024-12-08 14:07:15 +0100

    feat: add cache control for AWS s3 objects

--- urban_odyssey/settings.py
+++ urban_odyssey/settings.py
@@ -187,11 +187,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS S3 Bucket Configuration
 if 'USE_AWS' in os.environ:
+	# Cache Control
+	AWS_S3_OBJECT_PARAMETERS = {
+		'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+		'CacheControl': 'max-age=94608000',
+	}
+
+	# Bucket Config
 	AWS_STORAGE_BUCKET_NAME = 'urban-odyssey'
 	AWS_S3_REGION_NAME = 'us-east-1'
 	AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 	AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 	AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+	# Static and Media Files
 	STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 	STATICFILES_LOCATION = 'static'
 	DEFAULT_FILE_STORAGE_STORAGE = 'custom_storages.MediaStorage'
